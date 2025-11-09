22150f67099372d0d11954f52fa0eaede7d8380d
Anas2332 <amunir2332@gmail.com>
2025-11-09 14:59:21 +0500

    Basic tic-tac-toe Setup

---
+++ src/Components/Player.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol, isActive }) {
+  const [ playerName, setPalyerName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPalyerName(event.target.value)
+  }
+
+  return (
+    <li className={isActive? 'active' :undefined}>
+      <span className="player">
+        {isEditing ? (
+          <input type="text" required value={playerName} onChange={handleChange}/>
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
